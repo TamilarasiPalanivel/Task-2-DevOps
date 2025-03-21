c8e07b240d0d3a0801af8fbe20bdf1ee8e832e6d
TAMILARASI P <tamilarasip.22cse@kongu.edu>
2025-03-21 19:32:32 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
+    docker login -u tamilarasipalanivel2005 -p dckr_pat_Vu7QlBQ-8uYQHdaZhuYrslnp3QE
+    docker tag test1 tamilarasipalanivel2005/devops
+    docker push tamilarasipalanivel2005/devops
     
